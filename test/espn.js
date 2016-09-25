7074a8fafb90c9e845f45e2efc0da065dfe2d6cc
Chris Parsons <chris@chris-parsons.com>
2016-09-24 19:14:11 -0700

    Fixing case sensitive issue.

--- test/espn.js
+++ test/espn.js
@@ -3,8 +3,15 @@ var assert = require("chai").assert;
 var espn = require("../lib/espn");
 
 describe('ESPN Utilities', function () {
-    it('Should return the next 5 games from a valid team abbreviation', function () {
+    it('Should return the next 5 games from a lowercase team abbreviation', function () {
         var teamName = 'la';
+        espn.next5(teamName).then(function (value) {
+            expect(value.schedule).to.have.lengthOf(5);
+        });
+    });
+
+    it('Should return the next 5 games from an uppercase team abbreviation', function () {
+        var teamName = 'MIA';
         espn.next5(teamName).then(function (value) {
             expect(value.schedule).to.have.lengthOf(5);
         });

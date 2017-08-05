106053a14fd1518b1cc9bda057aa8d224429a464
Chris Parsons <chris.parsons@pccmarkets.com>
2017-08-05 09:10:14 -0700

    Fixing tests

--- test/espn.test.js
+++ test/espn.test.js
@@ -1,24 +1,23 @@
-﻿var expect = require("chai").expect;
-var assert = require("chai").assert;
-var espn = require("../lib/espn");
+﻿const expect = require("chai").expect;
+const espn = require("../lib/espn");
 
 describe('ESPN Utilities', function () {
     it('Should return the next 5 games from a lowercase team abbreviation', function () {
-        var teamName = 'la';
+        let teamName = 'la';
         espn.next5(teamName).then(function (value) {
             expect(value.schedule).to.have.lengthOf(5);
         });
     });
 
     it('Should return the next 5 games from an uppercase team abbreviation', function () {
-        var teamName = 'MIA';
+        let teamName = 'MIA';
         espn.next5(teamName).then(function (value) {
             expect(value.schedule).to.have.lengthOf(5);
         });
     });
 
     it('Should return the next 5 games from a valid team mascot', function () {
-        var teamName = 'Seahawks';
+        let teamName = 'Seahawks';
         espn.next5(teamName).then(function (value) {
             expect(value.schedule).to.have.lengthOf(5);
         });

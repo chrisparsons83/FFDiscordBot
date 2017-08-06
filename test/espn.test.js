72bb03d2f0667b2db9301132988f7a7fc064312a
Chris Parsons <chris.parsons@pccmarkets.com>
2017-08-06 10:25:17 -0700

    Fixing all the tests!

--- test/espn.test.js
+++ test/espn.test.js
@@ -3,23 +3,30 @@ const espn = require("../lib/espn");
 
 describe('ESPN Utilities', function () {
     it('Should return the next 5 games from a lowercase team abbreviation', function () {
-        let teamName = 'la';
-        espn.next5(teamName).then(function (value) {
+        let teamName = 'lar';
+        return espn.next5(teamName).then(function (value) {
             expect(value.schedule).to.have.lengthOf(5);
         });
     });
 
     it('Should return the next 5 games from an uppercase team abbreviation', function () {
         let teamName = 'MIA';
-        espn.next5(teamName).then(function (value) {
+        return espn.next5(teamName).then(function (value) {
             expect(value.schedule).to.have.lengthOf(5);
         });
     });
 
     it('Should return the next 5 games from a valid team mascot', function () {
         let teamName = 'Seahawks';
-        espn.next5(teamName).then(function (value) {
+        return espn.next5(teamName).then(function (value) {
             expect(value.schedule).to.have.lengthOf(5);
+        });
+    });
+
+    it('Should return an error with an invalid team abbreviation', function () {
+        let teamName = 'abc';
+        return espn.next5(teamName).catch(function (value) {
+            expect(value).to.equal("No team name was found.");
         });
     });
 });

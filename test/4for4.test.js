15317a29324f06550d3420b09edbd865987c6c23
Chris Parsons <chris.parsons@pccmarkets.com>
2017-08-14 09:27:43 -0700

    Fix lowercase name issue in player search for 4for4 adp

--- test/4for4.test.js
+++ test/4for4.test.js
@@ -4,6 +4,7 @@ const moment = require("moment");
 
 describe('4for4 Utilities', function () {
     let validPlayer = "David Johnson";
+    let validLowercasePlayer = "david johnson";
     let invalidPlayer = "Bob Dole";
 
     it('Should return a date in the past for the last updated date', () => {
@@ -13,38 +14,45 @@ describe('4for4 Utilities', function () {
         });
     });
 
-    it('Should return the team name for a player', () => {
+    it('Should return the team name for a valid player', () => {
         return lib4for4.getADP(validPlayer).then(function (value) {
             expect(value.Team).to.be.a('string');
             expect(value.Team).to.equal('ARI');
         });
     });
 
-    it('Should return the ESPN ranking for a player', () => {
+    it('Should return the ESPN ranking for a valid player', () => {
         return lib4for4.getADP(validPlayer).then(function (value) {
             expect(value.ADP.ESPN).to.be.a('number');
             expect(value.ADP.ESPN).to.be.greaterThan(0);
         });
     });
 
-    it('Should return the Yahoo ranking for a player', () => {
+    it('Should return the Yahoo ranking for a valid player', () => {
         return lib4for4.getADP(validPlayer).then(function (value) {
             expect(value.ADP.Yahoo).to.be.a('number');
             expect(value.ADP.Yahoo).to.be.greaterThan(0);
         });
     });
 
-    it('Should return the MFL ranking for a player', () => {
+    it('Should return the MFL ranking for a valid player', () => {
         return lib4for4.getADP(validPlayer).then(function (value) {
             expect(value.ADP.MFL).to.be.a('number');
             expect(value.ADP.MFL).to.be.greaterThan(0);
         });
     });
 
-    it('Should return the NFL ranking for a player', () => {
+    it('Should return the NFL ranking for a valid player', () => {
         return lib4for4.getADP(validPlayer).then(function (value) {
             expect(value.ADP.NFL).to.be.a('number');
             expect(value.ADP.NFL).to.be.greaterThan(0);
+        });
+    });
+
+    it('Should return the rankings for a valid player using all lowercase', () => {
+        return lib4for4.getADP(validLowercasePlayer).then(function (value) {
+            expect(value.LastUpdated).to.be.a('string');
+            expect(value.LastUpdated).to.not.be.empty;
         });
     });
 

954ca7d1c2ad5896ff74c86052d4fdefc4039172
Chris Parsons <chrisparsons1127@gmail.com>
2017-08-13 18:18:19 -0700

    Adding ADP command, making README more comprehensive, fixing typo in teamlisting.

---
+++ test/4for4.test.js
@@ -0,0 +1,56 @@
+﻿const expect = require("chai").expect;
+const lib4for4 = require("../lib/4for4");
+const moment = require("moment");
+
+describe('4for4 Utilities', function () {
+    let validPlayer = "David Johnson";
+    let invalidPlayer = "Bob Dole";
+
+    it('Should return a date in the past for the last updated date', () => {
+        return lib4for4.getADP(validPlayer).then(function (value) {
+            expect(value.LastUpdated).to.be.a('string');
+            expect(value.LastUpdated).to.not.be.empty;
+        });
+    });
+
+    it('Should return the team name for a player', () => {
+        return lib4for4.getADP(validPlayer).then(function (value) {
+            expect(value.Team).to.be.a('string');
+            expect(value.Team).to.equal('ARI');
+        });
+    });
+
+    it('Should return the ESPN ranking for a player', () => {
+        return lib4for4.getADP(validPlayer).then(function (value) {
+            expect(value.ADP.ESPN).to.be.a('number');
+            expect(value.ADP.ESPN).to.be.greaterThan(0);
+        });
+    });
+
+    it('Should return the Yahoo ranking for a player', () => {
+        return lib4for4.getADP(validPlayer).then(function (value) {
+            expect(value.ADP.Yahoo).to.be.a('number');
+            expect(value.ADP.Yahoo).to.be.greaterThan(0);
+        });
+    });
+
+    it('Should return the MFL ranking for a player', () => {
+        return lib4for4.getADP(validPlayer).then(function (value) {
+            expect(value.ADP.MFL).to.be.a('number');
+            expect(value.ADP.MFL).to.be.greaterThan(0);
+        });
+    });
+
+    it('Should return the NFL ranking for a player', () => {
+        return lib4for4.getADP(validPlayer).then(function (value) {
+            expect(value.ADP.NFL).to.be.a('number');
+            expect(value.ADP.NFL).to.be.greaterThan(0);
+        });
+    });
+
+    it('Should return an error with an invalid player', () => {
+        return lib4for4.getADP(invalidPlayer).catch(function (value) {
+            expect(value).to.equal("No player found. Please check your spelling, or he may be outside the top 300.");
+        });
+    });
+});

f8e751f848494657d8e81146298cdce9d3a80e68
Chris Parsons <chrisparsons1127@gmail.com>
2017-08-18 21:37:09 -0700

    Cleaning up some files and doing some optimation in preparation for eslint.

--- test/4for4.test.js
+++ test/4for4.test.js
@@ -1,16 +1,15 @@
-﻿const expect = require("chai").expect;
-const request = require("request");
-const lib4for4 = require("../lib/4for4");
-const moment = require("moment");
-const sinon = require("sinon");
+﻿const expect = require('chai').expect;
+const request = require('request');
+const lib4for4 = require('../lib/4for4');
+const sinon = require('sinon');
 
-describe('4for4 Utilities', function () {
-    let validPlayer = "David Johnson";
-    let validLowercasePlayer = "david johnson";
-    let invalidPlayer = "Bob Dole";
+describe('4for4 Utilities', () => {
+    const validPlayer = 'David Johnson';
+    const validLowercasePlayer = 'david johnson';
+    const invalidPlayer = 'Bob Dole';
 
     before((done) => {
-        let url = 'https://www.4for4.com/fantasy-football/adp?paging=0';
+        const url = 'https://www.4for4.com/fantasy-football/adp?paging=0';
         request.get(url, (error, response, html) => {
             sinon.stub(request, 'get')
                 .yields(null, null, html);
@@ -23,58 +22,42 @@ describe('4for4 Utilities', function () {
         done();
     });
 
-    it('Should return a date in the past for the last updated date', () => {
-        return lib4for4.getADP(validPlayer).then(function (value) {
-            expect(value.LastUpdated).to.be.a('string');
-            expect(value.LastUpdated).to.not.be.empty;
-        });
-    });
+    it('Should return a date in the past for the last updated date', () => lib4for4.getADP(validPlayer).then((value) => {
+        expect(value.LastUpdated).to.be.a('string');
+        expect(value.LastUpdated).to.not.be.empty;
+    }));
 
-    it('Should return the team name for a valid player', () => {
-        return lib4for4.getADP(validPlayer).then(function (value) {
-            expect(value.Team).to.be.a('string');
-            expect(value.Team).to.equal('ARI');
-        });
-    });
+    it('Should return the team name for a valid player', () => lib4for4.getADP(validPlayer).then((value) => {
+        expect(value.Team).to.be.a('string');
+        expect(value.Team).to.equal('ARI');
+    }));
 
-    it('Should return the ESPN ranking for a valid player', () => {
-        return lib4for4.getADP(validPlayer).then(function (value) {
-            expect(value.ADP.ESPN).to.be.a('number');
-            expect(value.ADP.ESPN).to.be.greaterThan(0);
-        });
-    });
+    it('Should return the ESPN ranking for a valid player', () => lib4for4.getADP(validPlayer).then((value) => {
+        expect(value.ADP.ESPN).to.be.a('number');
+        expect(value.ADP.ESPN).to.be.greaterThan(0);
+    }));
 
-    it('Should return the Yahoo ranking for a valid player', () => {
-        return lib4for4.getADP(validPlayer).then(function (value) {
-            expect(value.ADP.Yahoo).to.be.a('number');
-            expect(value.ADP.Yahoo).to.be.greaterThan(0);
-        });
-    });
+    it('Should return the Yahoo ranking for a valid player', () => lib4for4.getADP(validPlayer).then((value) => {
+        expect(value.ADP.Yahoo).to.be.a('number');
+        expect(value.ADP.Yahoo).to.be.greaterThan(0);
+    }));
 
-    it('Should return the MFL ranking for a valid player', () => {
-        return lib4for4.getADP(validPlayer).then(function (value) {
-            expect(value.ADP.MFL).to.be.a('number');
-            expect(value.ADP.MFL).to.be.greaterThan(0);
-        });
-    });
+    it('Should return the MFL ranking for a valid player', () => lib4for4.getADP(validPlayer).then((value) => {
+        expect(value.ADP.MFL).to.be.a('number');
+        expect(value.ADP.MFL).to.be.greaterThan(0);
+    }));
 
-    it('Should return the NFL ranking for a valid player', () => {
-        return lib4for4.getADP(validPlayer).then(function (value) {
-            expect(value.ADP.NFL).to.be.a('number');
-            expect(value.ADP.NFL).to.be.greaterThan(0);
-        });
-    });
+    it('Should return the NFL ranking for a valid player', () => lib4for4.getADP(validPlayer).then((value) => {
+        expect(value.ADP.NFL).to.be.a('number');
+        expect(value.ADP.NFL).to.be.greaterThan(0);
+    }));
 
-    it('Should return the rankings for a valid player using all lowercase', () => {
-        return lib4for4.getADP(validLowercasePlayer).then(function (value) {
-            expect(value.LastUpdated).to.be.a('string');
-            expect(value.LastUpdated).to.not.be.empty;
-        });
-    });
+    it('Should return the rankings for a valid player using all lowercase', () => lib4for4.getADP(validLowercasePlayer).then((value) => {
+        expect(value.LastUpdated).to.be.a('string');
+        expect(value.LastUpdated).to.not.be.empty;
+    }));
 
-    it('Should return an error with an invalid player', () => {
-        return lib4for4.getADP(invalidPlayer).catch(function (value) {
-            expect(value).to.equal("No player found. Please check your spelling, or he may be outside the top 300.");
-        });
-    });
+    it('Should return an error with an invalid player', () => lib4for4.getADP(invalidPlayer).catch((value) => {
+        expect(value).to.equal('No player found. Please check your spelling, or he may be outside the top 300.');
+    }));
 });

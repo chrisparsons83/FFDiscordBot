3d48d4605d8d4bcf86daea24e2d24c8c5447f334
Chris Parsons <chrisparsons1127@gmail.com>
2017-08-19 17:30:51 -0700

    Cleaning up app.js and adding .vscode to gitignore

--- app.js
+++ app.js
@@ -5,25 +5,25 @@ const commands = require('./lib/commands');
 const bot = new Discord.Client();
 
 bot.on('guildMemberAdd', (member) => {
-    member.guild.defaultChannel.send(`Welcome to the /r/fantasyfootball discord server, ${member}`);
+  member.guild.defaultChannel.send(`Welcome to the /r/fantasyfootball discord server, ${member}`);
 });
 
 bot.on('message', (msg) => {
-    if (msg.content.startsWith('!')) {
-        let messageCommand = '';
-        let messageArgsArray = [];
-        [messageCommand, ...messageArgsArray] = msg.content.toString().split(' ').map(value => value.trim()).slice(0);
-        const messageArgs = messageArgsArray.join(' ');
-        const validCommand = Object.prototype.hasOwnProperty.call(commands, messageCommand);
+  if (msg.content.startsWith('!')) {
+    let messageCommand = '';
+    let messageArgsArray = [];
+    [messageCommand, ...messageArgsArray] = msg.content.toString().split(' ').map(value => value.trim()).slice(0);
+    const messageArgs = messageArgsArray.join(' ');
+    const validCommand = Object.prototype.hasOwnProperty.call(commands, messageCommand);
 
-        if (validCommand) {
-            commands[messageCommand](messageArgs).then((response) => {
-                msg.channel.send(response);
-            }).catch((err) => {
-                msg.channel.send(err);
-            });
-        }
+    if (validCommand) {
+      commands[messageCommand](messageArgs).then((response) => {
+        msg.channel.send(response);
+      }).catch((err) => {
+        msg.channel.send(err);
+      });
     }
+  }
 });
 
 bot.login(config.DiscordAPIToken);

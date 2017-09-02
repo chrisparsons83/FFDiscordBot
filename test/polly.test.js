98239404cee08088c4a89267ef2ee198d3e364be
Terry Chong <tc216997@gmail.com>
2017-09-03 06:51:51 +0800

    remove api call in polly test

--- test/polly.test.js
+++ test/polly.test.js
@@ -3,13 +3,6 @@ const commands = require('../lib/commands.js');
 
 describe('Poll command test ', () => {
 
-  it('Should return an url from strawpoll api call.', () => {
-    const input = 'Is lamar miller a bust? yes | no | maybe so. | I hate you';
-    return commands['!poll'](input).then((res) => {
-      expect(res).to.match(/http?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}/);
-    });
-  });
-
   it('Should complain when there are not enough answers.', () => {
     const input = 'Is lamar miller a bust? | yes ';
     return commands['!poll'](input)

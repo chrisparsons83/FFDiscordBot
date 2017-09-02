02eb4999690f8b121c1b5da525c2e81fd4204349
Terry Chong <tc216997@gmail.com>
2017-09-03 04:35:34 +0800

    readded test files

---
+++ test/nflteams.test.js
@@ -0,0 +1,23 @@
+﻿const expect = require('chai').expect;
+const nflteams = require('../lib/nflteams');
+
+describe('NFL Teams Utility', () => {
+  const validTeam = 'Seahawks';
+  const ambiguousTeam = 'New York';
+  const invalidTeam = 'Tokyo';
+
+  it('Should return a team object for a valid team', () => nflteams.getTeam(validTeam, (err, teamObject) => {
+    expect(err).to.be.empty;
+    expect(teamObject.name).to.equal('Seattle Seahawks');
+  }));
+
+  it('Should return an error for an ambiguous location', () => nflteams.getTeam(ambiguousTeam, (err, teamObject) => {
+    expect(err).to.equal('No team name was found.');
+    expect(teamObject).to.be.empty;
+  }));
+
+  it('Should return an error for an invalid team', () => nflteams.getTeam(invalidTeam, (err, teamObject) => {
+    expect(err).to.equal('No team name was found.');
+    expect(teamObject).to.be.empty;
+  }));
+});

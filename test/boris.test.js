734da71cfbec8a54e826f2424fceebd2064625ee
Terry Chong <tc216997@gmail.com>
2017-09-07 21:07:12 +0800

    added flex for wdis, removed scoring format requirement for non scoring required position, reject invalid players, updated readme

--- test/boris.test.js
+++ test/boris.test.js
@@ -1,7 +1,8 @@
 const expect = require('chai').expect;
 const boris = require('../lib/borischen.js');
+const commands = require('../lib/commands.js');
 
-describe('Boris library', () => {
+describe('Boris library and commands', () => {
   it('getTier should return wide receiver tiers with full scoring format object', () => {
     const position = 'wr';
     const scoring = 'full';
@@ -35,6 +36,46 @@ describe('Boris library', () => {
       expect(value.position).to.be.a.string;
       expect(value.url).to.be.a.string;
       expect(value.thumbnail).to.be.a.string;
+    });
+  });
+
+  it('Non-scoring format required should return an embed object', () => {
+    const string = 'qb';
+    return commands['!boris'](string).then((obj) => {
+      expect(Object.keys(obj).length).to.equal(1);
+      expect(Object.keys(obj.embed).length).to.equal(5);
+      expect(obj.embed.title).to.be.not.empty;
+      expect(obj.embed.color).to.be.not.empty;
+      expect(obj.embed.url).to.be.not.empty;
+      expect(obj.embed.thumbnail).to.be.not.empty;
+      expect(obj.embed.fields).to.be.not.empty;
+    });
+  });
+
+  it('Should complain when the position is invalid', () => {
+    const string = 'qr';
+    return commands['!boris'](string).then(() => {
+    }).catch((err) => {
+      expect(err).to.be.a.string;
+      expect(err).to.equal('Invalid position entered.');
+    });
+  });
+
+  it('Should complain when missing scoring format if the position requires it', () => {
+    const string = 'wr';
+    return commands['!boris'](string).then(() => {
+    }).catch((err) => {
+      expect(err).to.be.a.string;
+      expect(err).to.equal('Invalid position/format.\nValid positions are ***qb, rb, wr, te, k , dst***.\nValid scoring formats are ***standard, half, full***');
+    });
+  });
+
+  it('Should complain when a invalid scoring format was entered', () => {
+    const string = 'wr, halfsf';
+    return commands['!boris'](string).then(() => {
+    }).catch((err) => {
+      expect(err).to.be.a.string;
+      expect(err).to.equal('Invalid position/format.\nValid positions are ***qb, rb, wr, te, k , dst***.\nValid scoring formats are ***standard, half, full***');
     });
   });
 

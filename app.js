3de3f8db11e7e04812c8cc12fd4b841b895fc023
Chris Parsons <chrisparsons1127@gmail.com>
2020-08-20 21:18:06 -0700

    Fixing app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const Discord = require('discord.js');
-const Knex = require('knex');
-const { Model } = require('objection');
+const Discord = require("discord.js");
+const Knex = require("knex");
+const { Model } = require("objection");
 
-const config = require('./config');
-const commands = require('./lib/commands');
-const commandsList = require('./stats/commandsList.json');
-const utilities = require('./lib/utilities');
-const knexConfig = require('./knexfile');
+const config = require("./config");
+const commands = require("./lib/commands");
+const commandsList = require("./stats/commandsList.json");
+const utilities = require("./lib/utilities");
+const knexConfig = require("./knexfile");
 
 // Database models
-const News = require('./models/News');
+const News = require("./models/News");
 
 // Initialize knex.
 const knex = Knex(knexConfig);
@@ -18,26 +18,29 @@ Model.knex(knex);
 // Initialize Discord Bot
 const bot = new Discord.Client();
 
-bot.on('message', (msg) => {
-  if (msg.content.startsWith('!')) {
-    let messageCommand = '';
+bot.on("message", (msg) => {
+  if (msg.content.startsWith("!")) {
+    let messageCommand = "";
     let messageArgsArray = [];
     [messageCommand, ...messageArgsArray] = msg.content
       .toString()
-      .split(' ')
-      .map(value => value.trim())
+      .split(" ")
+      .map((value) => value.trim())
       .slice(0);
-    const messageArgs = messageArgsArray.join(' ');
+    const messageArgs = messageArgsArray.join(" ");
 
     // Make sure message command is case insensitive, but rest of string should matter on case.
     messageCommand = messageCommand.toLowerCase();
 
     // Confirm that the command called exists.
-    const validCommand = Object.prototype.hasOwnProperty.call(commands, messageCommand);
+    const validCommand = Object.prototype.hasOwnProperty.call(
+      commands,
+      messageCommand
+    );
     // check to see if there was a minor typo in the command
     const closeEnough = utilities.checkMinorTypo(
       messageCommand.slice(1, messageCommand.length),
-      commandsList,
+      commandsList
     );
     // Create an object with data to send to the commands
     const messageObject = {
@@ -46,28 +49,36 @@ bot.on('message', (msg) => {
       channel: msg.channel,
     };
     if (validCommand) {
-      commands[messageCommand](messageObject).then((response) => {
-        // Check to see if there's a channel passed as a key/value in the object.
-        if (response.message) {
-          // If so, this is the new format for the bot.
-          // If there's a channel name stored in the object, then we want to send it to
-          // that particular channel if it exists. Otherwise
-          // TODO: When we add a database to this, this needs to be able to be configured
-          // on the server level.
-          if (response.channelName) {
-            // finds the channel object
-            const channel = msg.member.guild.channels.find(x => x.name === response.channelName);
-            if (!channel) return;
-
-            //sends message to prediction channel
-            channel.send(response.message);
-            // !prediction specific function call, sends an eightball response to the channel that prediction was called
-            if (response.channelName === 'predictions') {
-              msg.channel.send(response.eightball)
+      commands[messageCommand](messageObject)
+        .then((response) => {
+          // Check to see if there's a channel passed as a key/value in the object.
+          if (response.message) {
+            // If so, this is the new format for the bot.
+            // If there's a channel name stored in the object, then we want to send it to
+            // that particular channel if it exists. Otherwise
+            // TODO: When we add a database to this, this needs to be able to be configured
+            // on the server level.
+            if (response.channelName) {
+              const channel = msg.member.guild.channels.find(
+                "name",
+                response.channelName
+              );
+              if (!channel) return;
+              channel.send(response.message);
             }
+          } else {
+            // Otherwise, just send the response.
+            // TODO: Convert all the old responses to the new
+            msg.channel.send(response);
           }
-        };
-      });
+        })
+        .catch((err) => {
+          if (err.message) {
+            msg.channel.send(err.message);
+          } else {
+            msg.channel.send(err);
+          }
+        });
     } else if (closeEnough) {
       msg.channel.send(`*Did you mean* \`${closeEnough}\` *?*`);
     }
@@ -76,11 +87,11 @@ bot.on('message', (msg) => {
 
 // We're going to use this as a way to setup a polling service to post news to
 // all of the servers we're connected to.
-bot.on('ready', () => {
+bot.on("ready", () => {
   // Start an interval loop that fires every 30 seconds.
   (async function updateNews() {
     // Check to see if there's any new news - we don't need to do anything if there isn't
-    const newNews = await News.query().where('is_published', '=', false);
+    const newNews = await News.query().where("is_published", "=", false);
 
     if (newNews.length > 0) {
       // Set those stories to published now
@@ -88,7 +99,7 @@ bot.on('ready', () => {
       // fix it at some point (that someone will likely be me).
       const publishedNews = await News.query()
         .patch({ is_published: true })
-        .where('is_published', '=', false);
+        .where("is_published", "=", false);
 
       // Check to see what servers are connected to this bot
       const guildList = bot.guilds.array();
@@ -96,7 +107,7 @@ bot.on('ready', () => {
       try {
         guildList.forEach((guild) => {
           // Check to see if they have an ff-news channel
-          const channel = guild.channels.find(val => val.name === 'ff-news');
+          const channel = guild.channels.find((val) => val.name === "ff-news");
           // if no channel, then skip this
           if (channel) {
             // If so, post each news story we need to publish
@@ -116,7 +127,7 @@ bot.on('ready', () => {
 
     // Pause for 30 seconds to give the server a break.
     setTimeout(updateNews, 30000);
-  }());
+  })();
 });
 
 bot.login(config.DiscordAPIToken);
